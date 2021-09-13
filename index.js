639be37337e5fc0a7469e04ce611152e0dcc00f4
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-09-13 18:12:06 +0200

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 
 const app = express();
-const PORT =  process.env.PORT || 3017;
 
+let smallPizzaCost =0;
+let mediumPizzaCost =0;
+let largePizzaCost =0;
+let overAllTotal=0;
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,22 +18,37 @@ app.use(express.static('public'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+var session = require('express-session')
 
-let counter = 0;
 
 app.get('/', function(req, res) {
 	res.render('index', {
-		counter
+		small:smallPizzaCost, medium: mediumPizzaCost, large:largePizzaCost
 	});
 });
 
-app.post('/count', function(req, res) {
-	counter++;
+app.post('/buy', function(req, res) {
+	let size = req.body.pizzaType
+	if(size == "small"){
+		smallPizzaCost += 34.99;
+		overAllTotal ++
+
+	}else if 
+	(size == "medium"){
+		mediumPizzaCost += 67.99;
+		overAllTotal ++
+	}else if 
+	(size == "large"){
+		largePizzaCost += 87.99;
+		overAllTotal ++
+	}
 	res.redirect('/')
 });
 
 
 // start  the server and start listening for HTTP request on the PORT number specified...
-app.listen(PORT, function() {
-	console.log(`App started on port ${PORT}`)
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, function () {
+    console.log("app started at", PORT)
 });
---
+++ script.js
@@ -0,0 +1,38 @@
+function pizzaCart() {
+    let totalCost = 0;
+    let smallPizza =0;
+    let mediumPizza =0;
+    let largePizza=0;
+
+
+  
+
+
+    function addPizza(quantity,size) {
+
+    }
+    function removePizza() {
+
+    }
+
+    function updateTotals() {
+
+    }
+    function grandTotal() {
+        return smallPizzaCost + mediumPizzaCost + LargePizzaCost
+
+    }
+
+
+
+
+
+    return {
+        addPizza,
+        removePizza,
+        updateTotals,
+        grandTotal,
+        
+    }
+
+}
